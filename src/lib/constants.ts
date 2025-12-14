5598157d8417d0e192f3f28d358806046ee26210
tah5721312 <tah5721312@gmail.com>
2025-12-14 23:54:33 +0200

    chore: update production domain to Vercel URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,12 +1,13 @@
 export const ARTICLE_PER_PAGE = 6;
 
-// const PRODUCTION_DOMAIN = "https://nextjs-tah.vercel.app";
+const PRODUCTION_DOMAIN = 'https://e-commerce-all.vercel.app/';
 // const PRODUCTION_DOMAIN = "http://192.168.58.63:3000";
-const PRODUCTION_DOMAIN = "http://localhost:3000";
+// const PRODUCTION_DOMAIN = "http://localhost:3000";
 
-const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+const DEVELOPMENT_DOMAIN = 'http://localhost:3000';
 // const DEVELOPMENT_DOMAIN = "http://192.168.58.63:3000"; // ← غيّر لـ IP جهاز السيرفر
 
-export const DOMAIN = process.env.NODE_ENV === 'production'
+export const DOMAIN =
+  process.env.NODE_ENV === 'production'
     ? PRODUCTION_DOMAIN
     : DEVELOPMENT_DOMAIN;
